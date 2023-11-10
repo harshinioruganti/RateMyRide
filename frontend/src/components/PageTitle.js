de8c62846a38fe32fc5233d20f0a8d3a0bd8cca1
Harshini Oruganti <harshinioruganti2020@gmail.com>
2023-11-10 00:50:24 -0500

    Fixing frontend files

--- frontend/src/components/PageTitle.js
+++ frontend/src/components/PageTitle.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import './PageTitle.css';
+import logo from '../logo.png';
 
 function PageTitle()
 {
-   return(
-     <h1 id="title">Rate My Ride</h1>
-   );
+  return(
+    <div className='title-container'>
+        <img src={logo} alt="Logo" className='logo' />
+        <h1 id="title" className="title">Welcome to RateMyRide!</h1>
+    </div>
+  );
 };
 
 export default PageTitle;
