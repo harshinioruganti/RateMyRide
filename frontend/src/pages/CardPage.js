c969e635b232f528f3c85d7b6be1d2deca1f8153
Harshini Oruganti <harshinioruganti2020@gmail.com>
2023-11-14 00:40:41 -0500

    Hardcoded ThemeParkList that goes to RideList

--- frontend/src/pages/CardPage.js
+++ frontend/src/pages/CardPage.js
@@ -3,42 +3,72 @@ import React, { useState } from 'react';
 import Header from '../components/Header';
 // import LoggedInName from '../components/LoggedInName';
 // import CardUI from '../components/CardUI';
+import ThemeParkList from '../components/ThemeParkList';
 import RideList from '../components/RideList';
 import RideDetails from '../components/RideDetails';
 
 const CardPage = () =>
 {
-    const rides = [
-        {
-          title: 'Ride 1',
-          description: 'A fun ride',
-          details: 'This is a detailed description of Ride 1.',
-        },
-        {
-          title: 'Ride 2',
-          description: 'An exciting adventure',
-          details: 'This is a detailed description of Ride 2.',
-        },
-        // Add more ride objects as needed
-      ];
+    const themeParks = [
+      {
+        id: 1,
+        name: 'Magic Kingdom',
+        description: 'The most magical place on earth.',
+      },
+      {
+        id: 2,
+        name: 'Epcot',
+        description: 'Explore the future and cultures around the world.',
+      },
+      // Add more theme park objects as needed
+    ];
 
+    const rides = [
+      {
+        id: 1,
+        themeParkId: 1,
+        title: 'Splash Mountain',
+        description: 'A thrilling water ride.',
+        details: 'This is a detailed description of Splash Mountain.',
+      },
+      {
+        id: 2,
+        themeParkId: 1,
+        title: 'Space Mountain',
+        description: 'A high-speed space-themed roller coaster.',
+        details: 'This is a detailed description of Space Mountain.',
+      },
+      // Add more ride objects as needed
+    ];
+
+    const [selectedThemePark, setSelectedThemePark] = useState(null);
     const [selectedRide, setSelectedRide] = useState(null);
 
-    const handleCardClick = (ride) => {
-        setSelectedRide(ride);
+    const handleThemeParkClick = (themePark) => {
+      setSelectedThemePark(themePark);
+      setSelectedRide(null);
+    };
+
+    const handleRideClick = (ride) => {
+      setSelectedRide(ride);
     };
 
     return (
-        <div className="app">
+      <div className="app">
         <Header />
         {selectedRide ? (
-            <RideDetails ride={selectedRide} onClose={() => setSelectedRide(null)} />
+          <RideDetails ride={selectedRide} onClose={() => setSelectedRide(null)} />
+        ) : selectedThemePark ? (
+          <RideList
+            rides={rides.filter((ride) => ride.themeParkId === selectedThemePark.id)}
+            onCardClick={handleRideClick}
+          />
         ) : (
-            <RideList rides={rides} onCardClick={handleCardClick} />
+          <ThemeParkList themeParks={themeParks} onCardClick={handleThemeParkClick} />
         )}
-        </div>
+      </div>
     );
-}
+};
 
 export default CardPage;
 
