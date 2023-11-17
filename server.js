d1e707bd2a6a87fbe92c371ae9fae5e547c081cd
cbreitbart <carsonbreitbart@gmail.com>
2023-11-17 15:35:11 -0500

    Repushing server.js
    git acting weird?

--- server.js
+++ server.js
@@ -54,9 +54,9 @@ if (process.env.NODE_ENV === 'production')
 app.post('/api/login', async (req, res, next) => 
 {
   // incoming: email, password
-  // outgoing: id, firstName, lastName, error
+  // outgoing: userID, firstName, lastName, log
 	
- var error = '';
+ var log = '';
 
   const { email, password } = req.body;
 
@@ -72,24 +72,29 @@ app.post('/api/login', async (req, res, next) =>
     id = results[0]._id;
     fn = results[0].FirstName;
     ln = results[0].LastName;
+    log = "Success."
+  }
+  else{
+    log = "Account doesn't exist."
   }
 
-  var ret = { id:id, firstName:fn, lastName:ln, error:''};
+
+  var ret = { userID:id, firstName:fn, lastName:ln, log:log};
   res.status(200).json(ret);
 });
 
 app.post('/api/register', async (req, res, next) => 
 {
   // incoming: firstName, lastName, email, password
-  // outgoing: error
+  // outgoing: log
 
-  var error = "";
+  var log = "";
 
   const { firstName, lastName, email, password } = req.body;
 
   const db = client.db('COP4331Cards');
 
-  /* // Set up auto increment user ID here
+  /*
   // Fetch the current maximum UserId
   const maxUserIdResult = await db.collection('Users').find({}, { sort: { UserId: -1 }, limit: 1 }).toArray();
   const currentMaxUserId = maxUserIdResult.length > 0 ? parseInt(maxUserIdResult[0].UserId) || 0 : 0;
@@ -106,28 +111,28 @@ app.post('/api/register', async (req, res, next) =>
   const results = await db.collection('Users').find({Email:email}).toArray();
   if(results.length > 0)
   {
-    error = "Account with this email already exists."
+    log = "Account with this email already exists."
   }
   else{
     var newAccount = {FirstName:firstName, LastName:lastName, Email:email, Password:password};
     db.collection('Users').insertOne(newAccount);
-    error = "Account created."
+    log = "Account created."
   }
 
-  var ret = { error:error };
+  var ret = { log:log };
   res.status(200).json(ret);
 });
 
 app.post('/api/searchRide', async (req, res, next) => {
 
   // incoming: rideName
-  // outgoing: rides, error
+  // outgoing: rides, log
 
   const { rideName } = req.body;
   const db = client.db('COP4331Cards');
 
-  var rides = [];
-  var error = '';
+  let rides = [];
+  let log = '';
 
   const results = await db.collection('Rides').find({ Ride: rideName }).toArray();
 
@@ -136,27 +141,27 @@ app.post('/api/searchRide', async (req, res, next) => {
       rideName: ride.Ride,
       description: ride.Description,
       themeParkId: ride.ThemeParkID,
-      rideId: ride._id, // Optionally include the ride ID in the response
-      error: error
+      rideId: ride._id // Optionally include the ride ID in the response
     }));
+    log = "Rides found."
   } 
   else {
-    error = 'No rides found with the specified name.';
+    log = "No rides found with the specified name.";
   }
 
-  res.status(200).json({ rides });
+  res.status(200).json({ rides, log });
 });
 
 
 app.post('/api/addRide', async (req, res, next) =>
 {
   // incoming: rideName, description, themeParkId
-  // outgoing: error
+  // outgoing: log
 	
   const { rideName, description, themeParkId } = req.body;
 	
   const newRide = {Ride:rideName,Description:description,ThemeParkID:themeParkId};
-  var error = '';
+  var log = '';
 
   try
   {
@@ -165,10 +170,10 @@ app.post('/api/addRide', async (req, res, next) =>
   }
   catch(e)
   {
-    error = e.toString();
+    log = e.toString();
   }
 
-  var ret = { error: error };
+  var ret = { log: log };
   res.status(200).json(ret);
 });
 
@@ -176,22 +181,22 @@ app.post('/api/addRide', async (req, res, next) =>
 app.post('/api/addReview', async (req, res, next) =>
 {
   // incoming: rideId, userId, thrill, theme, length, overall, review
-  // outgoing: error
+  // outgoing: log
 	
   const { rideId, userId, thrill, theme, length, overall, review } = req.body;
-	var error = '';
+  var log = '';
 
   const db = client.db('COP4331Cards');
   const results = await db.collection('Reviews').find({RideID:rideId,UserID:userId}).toArray();
   if(results.length > 0)
   {
-    error = "You have already reveiwed this ride! Either edit existing review or delete it before creating a new one."
+    log = "You have already reveiwed this ride! Either edit existing review or delete it before creating a new one.";
   }
   else
   {
     const newReview = {RideID:rideId,UserID:userId,Thrill:thrill,Theme:theme,Length:length,Overall:overall,Review:review};
     const result = db.collection('Reviews').insertOne(newReview);
-    error = "Rewiew added. Thank you!"
+    log = "Rewiew added. Thank you!";
   }
 
   var ret = { error: error };
@@ -204,7 +209,7 @@ app.post('/api/deleteReview', async (req, res, next) =>
   // outgoing: log
 	
   var log = "";
-  const { reviewId } = req.body; // Assuming you send the review ID in the request body
+  const { reviewId } = req.body;
 
   const db = client.db('COP4331Cards');
 
@@ -216,7 +221,7 @@ app.post('/api/deleteReview', async (req, res, next) =>
     log = "Review deleted.";
   }
   else{
-    log = "Review doesn't exist."
+    log = "Review doesn't exist.";
   }
   // Review was successfully deleted
   var ret = { log:log };
@@ -227,11 +232,11 @@ app.post('/api/deleteReview', async (req, res, next) =>
 app.post('/api/avgScores', async (req, res, next) =>
 {
   // incoming: rideId
-  // outgoing: thrillAvg, themeAvg, lengthAvg, overallAvg, error
+  // outgoing: thrillAvg, themeAvg, lengthAvg, overallAvg, log
 	
   const { rideId } = req.body;
 	
-  var error = "";
+  var log = "";
 
   const db = client.db('COP4331Cards');
   const results = await db.collection('Reviews').find({RideID:rideId}).toArray();
@@ -252,46 +257,175 @@ app.post('/api/avgScores', async (req, res, next) =>
     }
     // Average scores
     thr /= n; the /= n; len /= n; ovr /= n;
+    log = "Success."
   }
   else
   {
-    error = "No reviews yet."
+    log = "No reviews yet."
   }
 
   // Convert back to strings always rounded to 1 decimal place
   thr = thr.toFixed(1); the = the.toFixed(1); len = len.toFixed(1); ovr = ovr.toFixed(1);
 
-  var ret = { thrillAvg:thr, themeAvg:the, lengthAvg:len, overallAvg:ovr, error:error};
+  var ret = { thrillAvg:thr, themeAvg:the, lengthAvg:len, overallAvg:ovr, log:log};
   res.status(200).json(ret);
 });
 
 app.post('/api/getAllThemeParks', async (req, res, next) => {
-  var error = "";
+  // incoming: 
+  // outgoing: allThemeParks, log
 
+  const {} = req.body;
+
+  var log = "";
   const db = client.db('COP4331Cards');
   const themeParks = await db.collection('ThemeParks').find({}).toArray();
 
-  if (themeParks.length > 0) 
-  {
+  if (themeParks.length > 0) {
     const mappedThemeParks = themeParks.map(themePark => ({
       themeParkId: themePark._id,
       themePark: themePark.ThemePark,
       city: themePark.City,
-      state: themePark.State,
-      error: error
+      state: themePark.State
       // Add other fields as needed
     }));
-
-    
+    log = "Success.";
+    var ret = { allThemeParks:mappedThemeParks, log:log};
+    res.status(200).json(ret);
   } 
-  else
-  {
-    error = "No theme parks found."
+  else{
+    log = "No theme parks found.";
+    var ret = { log:log };
+    res.status(200).json(ret);
   }
-
-  res.status(200).json(mappedThemeParks);
 });
 
+app.post('/api/getRides', async (req, res, next) => 
+{
+  // incoming: themeParkId
+  // outgoing: rideList, log
+	
+  const { themeParkId } = req.body;
+
+  const db = client.db('COP4331Cards');
+  const results = await db.collection('Rides').find({ThemeParkID:themeParkId}).toArray();
+
+  let rides = [];
+  let log = '';
+
+  if( results.length > 0 )
+  {
+    rides = results.map(ride => ({
+      rideName: ride.Ride,
+      description: ride.Description,
+      themeParkId: ride.ThemeParkID,
+      rideId: ride._id
+    }));
+    log = "Rides found."
+  }
+  else {
+    log = "No rides found at that theme park.";
+  }
+
+  var ret = { rideList:rides, log:log};
+  res.status(200).json(ret);
+});
+
+app.post('/api/getRideInfo', async (req, res, next) => 
+{
+  // incoming: rideId
+  // outgoing: rideName, description, themeParkId, log
+	
+ var log = '';
+
+  const { rideId } = req.body;
+
+  const db = client.db('COP4331Cards');
+  const results = await db.collection('Rides').find({_id:new ObjectId(rideId)}).toArray();
+
+  if( results.length > 0 )
+  {
+    rideName = results[0].Ride;
+    description = results[0].Description;
+    themeParkId = results[0].ThemeParkID;
+    log = "Success."
+  }
+  else{
+    log = "Ride doesn't exist."
+  }
 
 
+  var ret = { rideName:rideName, description:description, themeParkId:themeParkId, log:log};
+  res.status(200).json(ret);
+});
+
+app.post('/api/getRideRating', async (req, res, next) =>
+{
+  // incoming: rideId
+  // outgoing: overallAvg, log
+	
+  const { rideId } = req.body;
+	
+  var log = "";
+
+  const db = client.db('COP4331Cards');
+  const results = await db.collection('Reviews').find({RideID:rideId}).toArray();
+
+  // Initialize vars to 0
+  var ovr = 0;
+
+  var n = results.length;
+  if( n > 0 )
+  {
+    // Add totals
+    for(let i = 0; i < n; i++)
+    {
+      ovr += parseInt(results[i].Overall);
+    }
+    // Average scores
+    ovr /= n;
+    log = "Success."
+  }
+  else
+  {
+    log = "No reviews yet."
+  }
+
+  // Convert back to strings always rounded to 1 decimal place
+  ovr = ovr.toFixed(1);
+
+  var ret = { overallAvg:ovr, log:log};
+  res.status(200).json(ret);
+});
+
+app.post('/api/getReviews', async (req, res, next) => {
+
+  // incoming: rideId
+  // outgoing: reviewList, log
+
+  const { rideId } = req.body;
+  const db = client.db('COP4331Cards');
+
+  let reviewList = [];
+  let log = '';
+
+  const results = await db.collection('Reviews').find({ RideID:rideId }).toArray();
+
+  if (results.length > 0) {
+    reviewList = results.map(review => ({
+      thrill: review.Thrill,
+      theme: review.Theme,
+      length: review.Length,
+      overall: review.Overall,
+      review: review.Review,
+      userId: review.UserID
+    }));
+    log = "Reviews found."
+  } 
+  else {
+    log = "No reviews yet.";
+  }
+
+  res.status(200).json({ reviewList, log });
+});
 })();
