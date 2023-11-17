7e329de3c2973cfb55e7e9305c2b0301ae40755d
vincent desk <vincentdp02@gmail.com>
2023-11-17 14:58:30 -0500

    added API functionality

--- MobileApp/Screens/Rides/Components/Accordion/ThemeParks/ThemeParkItem.js
+++ MobileApp/Screens/Rides/Components/Accordion/ThemeParks/ThemeParkItem.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { rides } from "../../../../../tests";
 // Components
 import RidesList from '../Rides/RideList';
 import SearchBar from '../../../../../Components/SearchBar/SearchBar';
@@ -15,7 +14,6 @@ const ThemeParkItem = ({ item, index }) => {
     // const backgroundColor = index % 3 === 0 ? '#fc7324' : index % 3 === 1 ? '#58ad93' : '#2fb4c2';
     const backgroundColor = index % 3 === 0 ? '#78c2e2' : index % 3 === 1 ? '#6eb476' : '#aed9e0';
 
-
     const handleThemeParkPress = () => {
         setActivePark(!activePark);
     }
@@ -23,9 +21,9 @@ const ThemeParkItem = ({ item, index }) => {
     return (
         <>
             <TouchableOpacity style={{ flex: 1 }} activeOpacity={ 0.7 } onPress={ handleThemeParkPress } >
-                <View style={{ ...styles.parkContainer, backgroundColor }} key={ item.ParkId } >
+                <View style={{ ...styles.parkContainer, backgroundColor }} key={ item.themeParkId } >
                     <View style={ styles.leftSide }>
-                        <Text style={ styles.themeParkName }>{ item.name }</Text>
+                        <Text style={ styles.themeParkName }>{ item.themePark }</Text>
                             <View style={ styles.locationContainer }>
                                 <Text style={ styles.cityState }>{ item.city }</Text>
                                 <Text style={ styles.spacing }>,</Text> 
@@ -46,9 +44,8 @@ const ThemeParkItem = ({ item, index }) => {
                         leftSideContainerStyle={ SearchBarStyle.secondaryLeftSideContainer }
                         placeholder={ 'SEARCH RIDE' }
                     />
-                    <RidesList 
-                        rides={ rides }
-                        targetThemeParkId={ item.ParkId }
+                    <RidesList
+                        targetThemeParkId={ item.themeParkId }
                         backgroundColor={ backgroundColor }
                     />
                 </>
